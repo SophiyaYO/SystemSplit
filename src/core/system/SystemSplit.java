2832d204d408ee0cd3b23c4bc6a48939b0e7c88a
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 18:35:31 +0300

    class a field and constructor added

--- src/core/system/SystemSplit.java
+++ src/core/system/SystemSplit.java
@@ -1,4 +1,17 @@
 package core.system;
 
+import core.model.hardware.Hardware;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SystemSplit {
+
+    private Map<String , Hardware> hardwareComponents;
+
+    public SystemSplit() {
+        this.hardwareComponents = new HashMap<>();
+    }
+
+
 }
