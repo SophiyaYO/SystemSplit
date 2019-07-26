ad05f371d7b87ccbdc6c2b02f4e118762f7a3e49
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 18:47:40 +0300

    refactored

--- src/core/command/RegisterPowerHardwareCommand.java
+++ src/core/command/RegisterPowerHardwareCommand.java
@@ -3,6 +3,7 @@ package core.command;
 import core.system.SystemSplit;
 
 public class RegisterPowerHardwareCommand {
+
     private SystemSplit systemSplit;
     private String name;
     private int capacity;
