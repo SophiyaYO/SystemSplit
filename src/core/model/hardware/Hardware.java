eb2ef51036a15dde29df78eb11bf89b8620a9e3e
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 18:17:22 +0300

    fields added

--- src/core/model/hardware/Hardware.java
+++ src/core/model/hardware/Hardware.java
@@ -1,4 +1,11 @@
 package core.model.hardware;
 
 public abstract class Hardware {
+
+    private String name;
+    private String type;
+    private int maxCapacity;
+    private int maxMemory;
+
+
 }
