819d713b7e8e7929f9359bd4f10cc6021b11312e
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 18:45:27 +0300

    fields added

--- src/core/command/RegisterPowerHardware.java
+++
@@ -1,4 +0,0 @@
-package core.command;
-
-public class RegisterPowerHardware  {
-}
---
+++ src/core/command/RegisterPowerHardwareCommand.java
@@ -0,0 +1,11 @@
+package core.command;
+
+import core.system.SystemSplit;
+
+public class RegisterPowerHardwareCommand {
+    private SystemSplit systemSplit;
+    private String name;
+    private int capacity;
+    private int memory;
+
+}
