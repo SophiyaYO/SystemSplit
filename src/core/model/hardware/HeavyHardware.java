626dfafa7967c702ba0b6ac73b7ed80b3766a519
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 18:28:41 +0300

    todo added

--- src/core/model/hardware/HeavyHardware.java
+++ src/core/model/hardware/HeavyHardware.java
@@ -1,6 +1,7 @@
 package core.model.hardware;
 
 public class HeavyHardware extends Hardware {
+    //TODO extract magic numbers as constants
 
     public HeavyHardware(String name, int maxCapacity, int maxMemory) {
         super(name, Type.HEAVY, maxCapacity, maxMemory);
