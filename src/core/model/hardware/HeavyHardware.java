33d90ebd142be068d8beddca3fcfc752d2eb42c8
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 18:21:10 +0300

    super() added

--- src/core/model/hardware/HeavyHardware.java
+++ src/core/model/hardware/HeavyHardware.java
@@ -1,4 +1,8 @@
 package core.model.hardware;
 
 public class HeavyHardware extends  Hardware {
+
+    public HeavyHardware(String name, int maxCapacity, int maxMemory) {
+        super(name, Type.HEAVY, maxCapacity, maxMemory);
+    }
 }
--- src/core/model/hardware/PowerHardware.java
+++ src/core/model/hardware/PowerHardware.java
@@ -1,4 +1,8 @@
 package core.model.hardware;
 
 public class PowerHardware extends Hardware {
+
+    public PowerHardware(String name, int maxCapacity, int maxMemory) {
+        super(name, Type.POWER, maxCapacity, maxMemory);
+    }
 }
