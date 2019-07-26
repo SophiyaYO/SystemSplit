2ad390b1cf65514bf81920804d40072b8016a1bf
SophYo <sophiya.dimitrova@gmail.com>
2019-07-26 18:20:08 +0300

    constructor and inner enum added

--- src/core/model/hardware/Hardware.java
+++ src/core/model/hardware/Hardware.java
@@ -3,9 +3,19 @@ package core.model.hardware;
 public abstract class Hardware {
 
     private String name;
-    private String type;
+    private Type type;
     private int maxCapacity;
     private int maxMemory;
 
+    public Hardware(String name, Type type, int maxCapacity, int maxMemory) {
+        this.name = name;
+        this.type = type;
+        this.maxCapacity = maxCapacity;
+        this.maxMemory = maxMemory;
+    }
 
+    protected enum Type {
+        POWER,
+        HEAVY
+    }
 }
