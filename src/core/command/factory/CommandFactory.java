75c1ce5aad45fb6de98b12b55b0da98d37c095cd
SophYo <sophiya.dimitrova@gmail.com>
2019-07-28 21:48:07 +0300

    little corrections

--- src/core/command/factory/CommandFactory.java
+++ src/core/command/factory/CommandFactory.java
@@ -13,9 +13,11 @@ public final class CommandFactory {
     public static Command buildCommand(String name, Object... args) {
 
         try {
-            Class commandClass = Class.forName(BASE_COMMAND_PACKAGE + COMMAND_SUFFIX);
+            Class commandClass = Class.forName(BASE_COMMAND_PACKAGE +name + COMMAND_SUFFIX);
+
             Constructor<Command> constructor = commandClass.getDeclaredConstructors()[0];
-            return constructor.newInstance(args);
+
+            return constructor.newInstance(new Object[]{args});
 
         } catch (ClassNotFoundException
                 | InstantiationException
