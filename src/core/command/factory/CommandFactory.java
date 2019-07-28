e03faef685969ab722aaaf4f9947241eeea2575d
SophYo <sophiya.dimitrova@gmail.com>
2019-07-28 21:15:51 +0300

    little fixes

--- src/core/command/factory/CommandFactory.java
+++ src/core/command/factory/CommandFactory.java
@@ -6,16 +6,17 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class CommandFactory {
-    private static final String BASE_COMMAND_PACkAGE = "core.command.";
+    private static final String BASE_COMMAND_PACKAGE = "core.command.";
     private static final String COMMAND_SUFFIX = "Command";
 
     @SuppressWarnings("unchecked")
     public Command buildCommand(String name, Object... args) {
 
         try {
-            Class commandClass = Class.forName(BASE_COMMAND_PACkAGE);
+            Class commandClass = Class.forName(BASE_COMMAND_PACKAGE + COMMAND_SUFFIX);
             Constructor<Command> constructor = commandClass.getDeclaredConstructors()[0];
-           return constructor.newInstance(args);
+            return constructor.newInstance(args);
+
         } catch (ClassNotFoundException
                 | InstantiationException
                 | IllegalAccessException
