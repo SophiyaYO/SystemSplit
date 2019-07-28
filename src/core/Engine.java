acc71520c0dbd1c7c13b87cb45f481d0e3341987
SophYo <sophiya.dimitrova@gmail.com>
2019-07-28 21:39:44 +0300

    run() code implementations

--- src/core/Engine.java
+++ src/core/Engine.java
@@ -1,9 +1,13 @@
 package core;
 
+import core.command.Command;
+import core.command.factory.CommandFactory;
 import core.inputOutput.reader.Reader;
 import core.system.SystemSplit;
 
 public class Engine {
+    private static final String END_COMMAND = "System Split";
+
     private SystemSplit systemSplit;
     private Reader reader;
 
@@ -14,6 +18,25 @@ public class Engine {
 
     public void run() {
 
+        String line = this.reader.readLine();
+
+        while (!END_COMMAND.equalsIgnoreCase(line)) {
+
+            String[] tokens = line.split("[\\(,\\s\\)]+");
+            String commandName = tokens[0];
+            Object[] params = new Object[tokens.length];
+            params[0] =this.systemSplit;
+            for (int i = 0; i < tokens.length; i++) {
+                params[i] = tokens[i];
+            }
+            Command command = CommandFactory.buildCommand(commandName, params);
+
+            command.execute();
+
+            line = this.reader.readLine();
+
+
+        }
     }
 
 }
