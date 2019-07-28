7323bae217f54eeaaf9a009870aaf04d7afd9915
SophYo <sophiya.dimitrova@gmail.com>
2019-07-28 23:09:50 +0300

    fixes

--- src/core/model/hardware/PowerHardware.java
+++ src/core/model/hardware/PowerHardware.java
@@ -4,8 +4,13 @@ import core.model.software.Software;
 
 public class PowerHardware extends Hardware {
 
+    private int usedCapacity;
+    private int usedMemory;
+
     public PowerHardware(String name, int maxCapacity, int maxMemory) {
         super(name, Type.POWER, maxCapacity, maxMemory);
+        this.usedCapacity = 0;
+        this.usedMemory = 0;
     }
 
     @Override
@@ -15,7 +20,7 @@ public class PowerHardware extends Hardware {
 
         baseCapacity -= (baseCapacity * 3) / 4;
 
-        return baseCapacity;
+        return baseCapacity - this.usedCapacity;
     }
 
     @Override
@@ -25,11 +30,12 @@ public class PowerHardware extends Hardware {
 
         baseMemory += (baseMemory * 3) / 4;
 
-        return baseMemory;
+        return baseMemory - this.usedMemory;
     }
 
     @Override
     protected void setUsedResourses(Software software) {
-
+        this.usedCapacity += software.getCapacityConsumption();
+        this.usedMemory += software.getMemoryConsumption();
     }
 }
