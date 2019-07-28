99f7ba64700fb08bb59c61529d36ac70f912cedb
SophYo <sophiya.dimitrova@gmail.com>
2019-07-28 21:22:45 +0300

    reading implementations

--- src/core/inputOutput/reader/ConsoleReader.java
+++ src/core/inputOutput/reader/ConsoleReader.java
@@ -1,12 +1,24 @@
 package core.inputOutput.reader;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ConsoleReader implements Reader {
-    private static final BufferedReader READER = null;
+    private static final BufferedReader READER;
+
+    static {
+        READER = new BufferedReader(new InputStreamReader(System.in));
+    }
 
     @Override
     public String readLine() {
-        return null;
+        try {
+            return READER.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
