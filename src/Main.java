2c820281e5836164e12c59a76e1796a6f01bfa40
SophYo <sophiya.dimitrova@gmail.com>
2019-07-28 21:41:34 +0300

    code implementations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import core.Engine;
+import core.inputOutput.reader.ConsoleReader;
+import core.inputOutput.reader.Reader;
+import core.system.SystemSplit;
+
 public class Main {
     public static void main(String[] args) {
+        Reader reader = new ConsoleReader();
 
+        SystemSplit systemSplit = new SystemSplit();
+
+        Engine engine = new Engine(systemSplit, reader);
+
+        engine.run();
     }
 }
