74c0b6e3ba05a209921d8d629ae675c2511a6d02
SophYo <sophiya.dimitrova@gmail.com>
2019-07-28 21:24:24 +0300

    constructor added

--- src/core/Engine.java
+++ src/core/Engine.java
@@ -1,15 +1,19 @@
 package core;
 
+import core.inputOutput.reader.Reader;
 import core.system.SystemSplit;
 
 public class Engine {
-private SystemSplit systemSplit;
+    private SystemSplit systemSplit;
+    private Reader reader;
 
-
+    public Engine(SystemSplit systemSplit, Reader reader) {
+        this.systemSplit = systemSplit;
+        this.reader = reader;
+    }
 
     public void run() {
 
     }
-
 
 }
